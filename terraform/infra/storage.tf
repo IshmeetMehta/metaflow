c5bb26d6584f518706074dbfd76fb25418b98678
Ishmeet Mehta <ishmeetm@google.com>
2026-06-03 22:09:00 +0000

    feat: add terraform configurations

--- terraform/infra/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" metaflow_storage_bucket {
-  provider = google-beta
-  name          = var.storage_bucket_name
-  location      = var.region
-  force_destroy = true # Allows deleting the bucket even if it contains Metaflow data
-
-  uniform_bucket_level_access = true
-}
--- terraform/services/argo_events.tf
+++
@@ -1,5 +0,0 @@
-module "argo_events" {
-  depends_on     = [null_resource.argo-quick-start-installation]
-  source         = "../../../common/terraform/argo_events"
-  jobs_namespace = "argo"
-}
